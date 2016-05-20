e6418fb21e5b26d98052b1be6e3d94896abb2b14
krauspe <kit02@peter-krauspe.de>
2016-05-20 15:03:20 +0200

    worked on test script. stiil not working properly.

--- bin/get_dn_from_vlan_config.sh
+++ bin/get_dn_from_vlan_config.sh
@@ -2,6 +2,9 @@
 
 # test script for function get_dn_from_vlan_config in
 # admin_get_status_list.sh
+basedir=/opt/dfs/tsctl2
+bindir=${basedir}/bin
+typeset -A VLAN_DN
 
 switch_vlan_prod_script=/home/sysman/tools/rem_pil/bin_ak/control_net_ak_psp.sh
 switch_vlan_dev_script=${bindir}/control_net_develop.sh
@@ -14,43 +17,51 @@ else
   switch_vlan_script=""
 fi
 
-fqdn2check=$1
-# fake cmdline arg
-fqdn2check=cwp4-s1.ak3.lgn.dfs.de  # exists
-#fqdn2check=cwp99-s1.ak3.lgn.dfs.de  # exists NOT
+# test input for function
+
+FQDNS2CHECK="cwp4-s1.ak3.lgn.dfs.de cwp3-s1.te1.lgn.dfs.de psp45-s1.te1.lgn.dfs.de psp8-mgt.lx3.lgn.dfs.de psp2-s1.te1.lgn.dfs.de"
 
 function get_dn_from_vlan_config
 {
 	fqdn_in=$1
 
 	if [[ -z $switch_vlan_script ]]; then
-		echo "DEBUG: $switch_vlan_script NOT FOUND"
+		#echo "DEBUG: $switch_vlan_script NOT FOUND"
 		echo ""
 		return
 	fi
-	# check if data available, if no run the script
+
 	if (( ${#VLAN_DN[*]} == 0 )); then
-		echo "running script $switch_vlan_script ..."
-		$switch_vlan_script -v | while read -r  fqdn descr switch port default_vlan current_vlan
+
+		#echo "No data available, running script $switch_vlan_script ..."
+
+		$switch_vlan_script -v | while read -r  fqdn descr switch port default_vlan current_vlan id
 		do
-			echo $fqdn $descr $switch $port $default_vlan $current_vlan
+			#echo $fqdn $descr $switch $port $default_vlan $current_vlan
 			current_dn=${current_vlan##Current_VLAN=}
 			VLAN_DN[$fqdn]=$current_dn
+			current_vlan=""
 		done
 	fi
 
-	if [[ -n ${VLAN_DN[$fqdn_in]} ]]; then
-		echo ${VLAN_DN[$fqdn_in]}
-	else
-		echo "DEBUG: NO entry for $fqdn_in FOUND"
+	dn_found=${VLAN_DN[$fqdn_in]}
+
+	if [[ -n $dn_found && $dn_found != *unknown* ]]; then
+		echo $dn_found
+	else 
 		echo ""
 	fi
 
 	return
 }
 
-dn=$(get_dn_from_vlan_config $fqdn2check)
 
-echo "dn=$dn"
+for fqdn2check in $FQDNS2CHECK
+do
+	# for test with echos
+	#get_dn_from_vlan_config $fqdn2check
 
-### FUNZT NET !!!!!!!!
+	dn=$(get_dn_from_vlan_config $fqdn2check)
+	echo "$fqdn2check: dn=$dn"
+done
+
