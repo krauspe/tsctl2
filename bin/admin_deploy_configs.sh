e38a13a788c78738e02c85fdadc39726cb1a95e5
krauspe <kit02@peter-krauspe.de>
2016-11-21 13:11:16 +0100

    new option '--cached': use cached data on resource nss(ses), don't collect anything (useful for reinstalled KNOWN clients only !!)

--- bin/admin_deploy_configs.sh
+++ bin/admin_deploy_configs.sh
@@ -2,21 +2,21 @@
 #
 # (c) Peter Krauspe 10/2015
 #
-# This script should run on an admin machine. It collects network configurations for 
-# all Resource NSC's of all resource domains and syncs theese to all Remote NSC's of all Remote Domains  
+# This script should run on an admin machine. It collects network configurations for
+# all Resource NSC's of all resource domains and syncs theese to all Remote NSC's of all Remote Domains
 #
 # So we can do an offline network reconfiguration on all resource NSC's fron all resource doamins
 #
 # changes:
 # 01.12.2015: - added java deployment for atcoach
 # 10.12.2015: - deploy ssh keys (aks) to ALL domain servers (not only remote) , because otherwise the "home nss" has no pwdless root acess on psp's
-#             - fixed bug on mkdir commmands for bindir and java deployment 
+#             - fixed bug on mkdir commmands for bindir and java deployment
 #
 # 05.01.2016: - scripts NEEDS options now, otherwise usage output only !!
 #             - create deploy options for selective deployments (more than java and wall_msg)
 #               example: deploy:all or deploy:config
-#             - add option to deploy to a single NSC rather than to all NSCs of a resource domain 
-#             - save ssh key deployment (backup old authorized keys) 
+#             - add option to deploy to a single NSC rather than to all NSCs of a resource domain
+#             - save ssh key deployment (backup old authorized keys)
 #             - changed option for ssh keys from "aks" to "keys"
 # 15.01.2016: - changed ssh key handling: deployment nss script ensures authorized key file with root public key and keys from all other nss'es
 #               (without backup, which is no longer necessary)
@@ -26,6 +26,7 @@
 #             - added calling script "admin_get_rpms_from_repo.sh" which copies rpms to var/install from Repo for local install on NSC's
 #             - changed cmdline handling: missing "deploy" string in first argumemt disables deploment to NSCs (independent of :<option>)
 # 14.04.2016: - minor bugfix in print out
+# 21.11.2016: - new option '--cached': use cached data on resource nss(ses), don't collect anything (useful for reinstalled KNOWN clients only !!)
 
 #
 #TODO: check changed cmdline handling for certain cases
@@ -61,7 +62,13 @@ if [[ $# < 2 ]]; then
    echo "   eg: $(basename $0) all|config|scripts|keys   all   # make everything due to action keyword but don't deploy to NSCs (PSPs) !!"
 	 echo
 	 echo "  --update-resources :  forces update of resource nsc list even if it exists, without this option an existing list will not be overwritten"
+	 echo "  --cached           :  uses cached data on nss of resource domain(s) for deployment"
    exit 1
+fi
+
+if [[ "$*" != *cached* && "$*" == *update-resources* ]]; then
+    echo "options '--cached' and '--update-resource' are mutual exclusive, exiting !!"
+    exit 1
 fi
 
 function check_host_alive
@@ -70,7 +77,7 @@ function check_host_alive
   ping -c 1 $host > /dev/null
   if (( $? == 0 )) ; then
     echo 1
-  else 
+  else
     echo 0
   fi
 }
@@ -78,12 +85,12 @@ function check_host_alive
 function create_remote_nsc_configs # create files on remote NSSes
 {
   typeset remote_domain_server=$1
-  # create <fqdn>.2step.vars and  remote_hn.list (Possibly add NO remote_hn.list at that point 
+  # create <fqdn>.2step.vars and  remote_hn.list (Possibly add NO remote_hn.list at that point
   cmd="ssh ${remote_domain_server} ${bindir}/nss_manage_remote_nsc.sh create_configs"
   $cmd
 }
 
-function get_remote_nsc_list  # returns the list. 
+function get_remote_nsc_list  # returns the list.
 {
   typeset remote_domain_server=$1
   typeset remote_dn=${remote_domain_server#*.}
@@ -151,8 +158,8 @@ if [[ $arg2 != "all" ]]; then
         target=${arg_hn}.${arg_dn}
         echo
         echo "OK found and reached $resdom_server_fqdn "
-        echo "Will create and deploy configs to all servers, "
-        echo "but deploy stuff ONLY to following NSC: $target "
+        #echo "Will create and deploy configs to all servers, "
+        echo "Deploy stuff ONLY to following NSC: $target "
         echo "Existence and reachability of $target is not checked at this point ! "
         echo "------------------------------------------------"
       else
@@ -171,84 +178,89 @@ fi
 
 ##############
 
-echo "\n<< Collect nsc configs from all RemoteDomainServers >>\n"
 
-[[ -f $remote_nsc_list_file     ]] && rm $remote_nsc_list_file 
-[[ -f  ${vardir}/aks ]] && rm  ${vardir}/aks
+if [[ "$*" != *cached* ]];then
+  echo "Use CACHED data for deployment due to comdline option !"
 
-# get uniq list of ALL servers
+    echo "\n<< Collect nsc configs from all RemoteDomainServers >>\n"
 
-AlleDomainServers=$(echo $ResourceDomainServers $RemoteDomainServers | sed 's/\s/\n/g' | sort -u)
+    [[ -f $remote_nsc_list_file     ]] && rm $remote_nsc_list_file
+    [[ -f  ${vardir}/aks ]] && rm  ${vardir}/aks
 
-echo "\n<< Deploy scripts to all Servers >>\n"
+    # get uniq list of ALL servers
 
-for domain_server in $AlleDomainServers 
-do
-  if [[ $domain_server != $(dnsdomainname) ]]; then
-    echo "  Deploy all scripts and configs to $domain_server"
-    ssh $domain_server "[[ -d $bindir ]] || mkdir -p $bindir " > /dev/null 2>&1
-    rsync -ahH ${confdir} ${domain_server}:$basedir > /dev/null 
-    rsync -ahH ${bindir}/nss* ${domain_server}:$bindir > /dev/null 
-    rsync -ahH ${bindir}/nsc* ${domain_server}:$bindir > /dev/null 
-    rsync -ahH ${bindir}/2step* ${domain_server}:$bindir > /dev/null 
-    #rsync -ahH ${basedir} ${domain_server}:/opt/dfs > /dev/null 
-  fi
-done
+    AlleDomainServers=$(echo $ResourceDomainServers $RemoteDomainServers | sed 's/\s/\n/g' | sort -u)
 
-echo "\n<< Create configs on RemoteDomainServers and collect on admin server >>\n"
+    echo "\n<< Deploy scripts to all Servers >>\n"
 
-for remote_domain_server in $RemoteDomainServers
-do
-   if [[ $(check_host_alive ${remote_domain_server}) == 0 ]]; then
-     echo "$remote_domain_server NOT REACHED, skipping !!"
-     continue
-   fi
-   remote_dn=${remote_domain_server#*.}
+    for domain_server in $AlleDomainServers
+    do
+      if [[ $domain_server != $(dnsdomainname) ]]; then
+        echo "  Deploy all scripts and configs to $domain_server"
+        ssh $domain_server "[[ -d $bindir ]] || mkdir -p $bindir " > /dev/null 2>&1
+        rsync -ahH ${confdir} ${domain_server}:$basedir > /dev/null
+        rsync -ahH ${bindir}/nss* ${domain_server}:$bindir > /dev/null
+        rsync -ahH ${bindir}/nsc* ${domain_server}:$bindir > /dev/null
+        rsync -ahH ${bindir}/2step* ${domain_server}:$bindir > /dev/null
+        #rsync -ahH ${basedir} ${domain_server}:/opt/dfs > /dev/null
+      fi
+    done
 
-   [[ -d ${vardir}/$remote_dn ]] || mkdir -p ${vardir}/$remote_dn
+    echo "\n<< Create configs on RemoteDomainServers and collect on admin server >>\n"
 
-   echo "  create remote nsc configs on $remote_domain_server"
-   create_remote_nsc_configs $remote_domain_server
+    for remote_domain_server in $RemoteDomainServers
+    do
+       if [[ $(check_host_alive ${remote_domain_server}) == 0 ]]; then
+         echo "$remote_domain_server NOT REACHED, skipping !!"
+         continue
+       fi
+       remote_dn=${remote_domain_server#*.}
 
-   for remote_nsc in $(get_remote_nsc_list $remote_domain_server)
-   do
-     echo "  copy $remote_nsc config from $remote_domain_server"
-     copy_remote_nsc_config $remote_domain_server $remote_nsc
-     echo $remote_nsc >> $remote_nsc_list_file
-   done
-done
-   
-echo "\n<< Get ssh public keys from all Servers >>\n"
+       [[ -d ${vardir}/$remote_dn ]] || mkdir -p ${vardir}/$remote_dn
 
-for domain_server in $AlleDomainServers 
-do
-   echo "  copy public key"
-   id_dsa_pub=$(ssh ${domain_server} cat /root/.ssh/id_dsa.pub)
-   echo $id_dsa_pub >> ${vardir}/aks
-   #authorized_keys
-done
+       echo "  create remote nsc configs on $remote_domain_server"
+       create_remote_nsc_configs $remote_domain_server
 
-echo "\n<< Deploy all configs to all Servers >>\n"
+       for remote_nsc in $(get_remote_nsc_list $remote_domain_server)
+       do
+         echo "  copy $remote_nsc config from $remote_domain_server"
+         copy_remote_nsc_config $remote_domain_server $remote_nsc
+         echo $remote_nsc >> $remote_nsc_list_file
+       done
+    done
 
-for domain_server in $AlleDomainServers
-do
-  if [[ $domain_server != $(dnsdomainname) ]]; then
-    echo "  Deploy all configs to $domain_server"
-    rsync -ahH ${vardir} ${domain_server}:$basedir > /dev/null 
-  fi
-done
+    echo "\n<< Get ssh public keys from all Servers >>\n"
+
+    for domain_server in $AlleDomainServers
+    do
+       echo "  copy public key"
+       id_dsa_pub=$(ssh ${domain_server} cat /root/.ssh/id_dsa.pub)
+       echo $id_dsa_pub >> ${vardir}/aks
+       #authorized_keys
+    done
+
+    echo "\n<< Deploy all configs to all Servers >>\n"
+
+    for domain_server in $AlleDomainServers
+    do
+      if [[ $domain_server != $(dnsdomainname) ]]; then
+        echo "  Deploy all configs to $domain_server"
+        rsync -ahH ${vardir} ${domain_server}:$basedir > /dev/null
+      fi
+    done
+fi
 
 #if ! [[ $arg1 != *deploy* && $arg1 == *all* || $arg1 == *config* || $arg1 == *scripts* || $arg1 == *keys* || $arg1 == *xinitrc* || $arg1 == *wall_msg* || $arg1 == *java* ]] ; then
 if  [[ $arg1 != *deploy* ]] ; then
   echo "WARNING: Deployment to NSCs is SKIPPED because of missing or wrong options !!"
   exit
 fi
-echo "\n<< Deploy all configs to all Resource NSC's >>\n"
+echo "\n<< Deploy configs Resource NSC(s) >>\n"
 
 for resource_domain_server in $TargetResourceDomainServers
 do
     # sync all configd to nsc's
-    echo "  syncing $resource_domain_server clients"
+    echo "  syncing $resource_domain_server client(s)"
     echo "    syncing tsctl2 stuff"
 
     if [[ $arg1 == *keys* || $arg1 == *all* ]] ; then
@@ -288,7 +300,7 @@ do
       ufa_dir=/opt/ufa
       cmd="mkdir -p ${ufa_dir}"
       # ufa_dir auf allen resource_domain_server anlegen
-      
+
       if [[ $resource_domain_server != $(dnsdomainname) ]]; then
           ssh $resource_domain_server $cmd  > /dev/null 2>&1
           # java auf alle resource_domain_server kopieren
